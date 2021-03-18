1f677b80410179d5c6103dc29e5d3993c2317d27
Francisco <pancho.luarca@gmail.com>
2021-03-18 01:17:49 -0600

    agregar cliente

--- src/conexion/conexion.java
+++ src/conexion/conexion.java
@@ -6,6 +6,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
+import vistas.ventanaGerente;
+import vistas.ventanaVendedor;
+import vistas.ventanaPrincipal;
 
 public class conexion {
     public static Connection con;
@@ -27,6 +30,7 @@ public class conexion {
         System.out.println("Desconectado de la base de Datos");
     }
     
+    
     public void iniciosesion(String usuario, String contraseña) throws SQLException{
         PreparedStatement ps = con.prepareStatement("select * from usuario where Nombre =? and Contraseña=?");
         ps.setString(1,usuario);
@@ -34,11 +38,31 @@ public class conexion {
         ResultSet rs = ps.executeQuery();
         if(rs.next()==true){
            JOptionPane.showMessageDialog(null,"Bienvenido") ;
-            System.out.println("el usuario es "+rs.getString(2));
-           
-        }else{
+           System.out.println("el usuario es "+rs.getString(2));
+           if("1".equals(rs.getString(4))){
+                System.out.println("usuario es gerente");
+                ventanaGerente gerente = new ventanaGerente();
+                gerente.setVisible(true);
+            }
+            if("2".equals(rs.getString(4))){
+                System.out.println("usuario es vendedor");
+                ventanaVendedor vendedor = new ventanaVendedor();
+                vendedor.setVisible(true);
+            }
+         }else{
             JOptionPane.showMessageDialog(null,"Datos Incorrectos") ;
         }
+    }
+    
+    public void agregarcliente(String Nombre, String Nit, String Telefono, String Direccion, String Tipo) throws SQLException{
+        PreparedStatement ps = con.prepareStatement("INSERT INTO cliente(Nombre, Nit, Telefono, Direccion, Tipo) VALUES(?,?,?,?,?)");
+        ps.setString(1, Nombre);
+        ps.setString(2, Nit);
+        ps.setString(3, Telefono);
+        ps.setString(4, Direccion);
+        ps.setString(5, Tipo);
+        ps.executeUpdate();
+        System.out.println("cliente agregado");        
     }
     
     
