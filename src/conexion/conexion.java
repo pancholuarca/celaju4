3368b52bd2ed24282d389db73876e789d79134a1
Francisco <pancho.luarca@gmail.com>
2021-03-17 23:53:44 -0600

    clase conexion

---
+++ src/conexion/conexion.java
@@ -0,0 +1,46 @@
+package conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class conexion {
+    public static Connection con;
+      
+    
+    public void conectar(){ 
+        try {
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/testcelaju","root","root1234");
+            if (con!=null){
+            System.out.println("Conectado a la base de datos");
+            }
+        } catch (SQLException e) {
+            System.out.println("Sin Conexion a base de datos");
+        }
+    }
+    
+    public void desconectar() throws SQLException{
+        con.close();
+        System.out.println("Desconectado de la base de Datos");
+    }
+    
+    public void iniciosesion(String usuario, String contraseña) throws SQLException{
+        PreparedStatement ps = con.prepareStatement("select * from usuario where Nombre =? and Contraseña=?");
+        ps.setString(1,usuario);
+        ps.setString(2,contraseña);
+        ResultSet rs = ps.executeQuery();
+        if(rs.next()==true){
+           JOptionPane.showMessageDialog(null,"Bienvenido") ;
+        }else{
+            JOptionPane.showMessageDialog(null,"Datos Incorrectos") ;
+        }
+    }
+    
+    
+    
+    
+    
+}
