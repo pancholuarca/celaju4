759cf2a8d6fd785d5a2249c70484f8fef3b56c5d
Francisco <pancho.luarca@gmail.com>
2021-04-22 01:16:55 -0600

    creacion de tabla de productos, boton agregar producto eliminar producto y limpiar

--- src/vistas/ventanaGerente.java
+++ src/vistas/ventanaGerente.java
@@ -2,9 +2,14 @@
 package vistas;
 
 import conexion.conexion;
+import static conexion.conexion.con;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 public class ventanaGerente extends javax.swing.JFrame {
 
@@ -12,6 +17,8 @@ public class ventanaGerente extends javax.swing.JFrame {
     public ventanaGerente() {
         initComponents();
         setLocationRelativeTo(null);
+        cn.conectar();
+        cargartabla();
     }
 
    
@@ -21,7 +28,7 @@ public class ventanaGerente extends javax.swing.JFrame {
 
         jTabbedPane1 = new javax.swing.JTabbedPane();
         jPanel1 = new javax.swing.JPanel();
-        btagregarp = new javax.swing.JButton();
+        btagregar = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
@@ -32,18 +39,20 @@ public class ventanaGerente extends javax.swing.JFrame {
         txtdescripcion = new javax.swing.JTextField();
         txtexistencia = new javax.swing.JTextField();
         txtprecio = new javax.swing.JTextField();
-        btbuscar = new javax.swing.JButton();
         bteliminar = new javax.swing.JButton();
         cbxlinea = new javax.swing.JComboBox<>();
         cbxbodega = new javax.swing.JComboBox<>();
+        btlimpiar = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tablaproducto = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        btagregarp.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
-        btagregarp.setText("Agregar Producto");
-        btagregarp.addActionListener(new java.awt.event.ActionListener() {
+        btagregar.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        btagregar.setText("Agregar Producto");
+        btagregar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btagregarpActionPerformed(evt);
+                btagregarActionPerformed(evt);
             }
         });
 
@@ -59,15 +68,50 @@ public class ventanaGerente extends javax.swing.JFrame {
 
         jLabel6.setText("Bodega:");
 
-        btbuscar.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
-        btbuscar.setText("Buscar");
-
         bteliminar.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
         bteliminar.setText("Eliminar");
+        bteliminar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bteliminarActionPerformed(evt);
+            }
+        });
 
         cbxlinea.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccione:", "Baterias", "Cables", "Conectores", "Pantallas", "Touch" }));
 
         cbxbodega.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seleccione:", "1", "2" }));
+
+        btlimpiar.setText("Limpiar");
+        btlimpiar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btlimpiarActionPerformed(evt);
+            }
+        });
+
+        tablaproducto.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null}
+            },
+            new String [] {
+                "Clave", "Descripción", "Línea", "Precio", "Bodega"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Double.class, java.lang.Integer.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        tablaproducto.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tablaproductoMouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(tablaproducto);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -97,52 +141,59 @@ public class ventanaGerente extends javax.swing.JFrame {
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(jLabel2)
                                 .addGap(8, 8, 8)))
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(btagregarp)
-                            .addComponent(btbuscar)
-                            .addComponent(bteliminar))
-                        .addGap(231, 231, 231))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(btagregar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(bteliminar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btlimpiar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel6)
                             .addComponent(jLabel5)
                             .addComponent(jLabel4))
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                        .addGap(339, 339, 339)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 453, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(18, 18, 18)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                    .addComponent(btagregar)
+                                    .addComponent(txtclave, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jLabel1)))
                         .addGap(18, 18, 18)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(btagregarp)
-                            .addComponent(txtclave, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel1)))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(txtdescripcion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btbuscar))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
-                    .addComponent(bteliminar)
-                    .addComponent(cbxlinea, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel4)
-                    .addComponent(txtexistencia, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel5)
-                    .addComponent(txtprecio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel6)
-                    .addComponent(cbxbodega, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jLabel2)
+                            .addComponent(txtdescripcion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(bteliminar))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel3)
+                            .addComponent(cbxlinea, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btlimpiar))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel4)
+                            .addComponent(txtexistencia, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel5)
+                            .addComponent(txtprecio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel6)
+                            .addComponent(cbxbodega, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 282, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -168,22 +219,85 @@ public class ventanaGerente extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btagregarpActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btagregarpActionPerformed
+    private void btagregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btagregarActionPerformed
         String Clave = txtclave.getText();
         String Descripcion = txtdescripcion.getText();
         String Linea = cbxlinea.getSelectedItem().toString();
         int Existencia = Integer.parseInt(txtexistencia.getText());
-        int Precio = Integer.parseInt(txtprecio.getText());
+        double Precio = Double.parseDouble(txtprecio.getText());
         int Bodega_idBodega = Integer.parseInt(cbxbodega.getSelectedItem().toString());;
+        
         try {
             // boton de agregar:
             cn.ingresarproducto(Clave, Descripcion, Linea, Existencia, Precio, Bodega_idBodega);
+            JOptionPane.showMessageDialog(null, "Producto Agregado");
+            cargartabla();
         } catch (SQLException ex) {
             Logger.getLogger(ventanaGerente.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-    }//GEN-LAST:event_btagregarpActionPerformed
+    }//GEN-LAST:event_btagregarActionPerformed
 
+    private void bteliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bteliminarActionPerformed
+        String Clave = txtclave.getText();
+        try {
+            cn.eliminarproducto(Clave);
+            cargartabla();
+        } catch (SQLException ex) {
+            Logger.getLogger(ventanaGerente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_bteliminarActionPerformed
+
+    private void tablaproductoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tablaproductoMouseClicked
+        // al seleccionar un producto
+        DefaultTableModel modelo = (DefaultTableModel) tablaproducto.getModel();
+        txtclave.setText(modelo.getValueAt(tablaproducto.getSelectedRow(), 1) +"");
+        txtdescripcion.setText(modelo.getValueAt(tablaproducto.getSelectedRow(), 2)+"");
+        txtexistencia.setText(modelo.getValueAt(tablaproducto.getSelectedRow(), 4)+"");
+        txtprecio.setText(modelo.getValueAt(tablaproducto.getSelectedRow(), 5)+"");
+        //txtbodega.setText(modelo.getValueAt(tablaproducto.getSelectedRow(), 6)+"");
+    }//GEN-LAST:event_tablaproductoMouseClicked
+
+    private void btlimpiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btlimpiarActionPerformed
+        // TODO add your handling code here:
+        limpiar();
+    }//GEN-LAST:event_btlimpiarActionPerformed
+
+     public void cargartabla(){
+       DefaultTableModel modelo = new DefaultTableModel();
+       modelo.addColumn("ID");
+       modelo.addColumn("Clave");
+       modelo.addColumn("Descripción");
+       modelo.addColumn("Línea");
+       modelo.addColumn("Existencia");
+       modelo.addColumn("Precio");
+       modelo.addColumn("Bodega");
+       cn.conectar();
+        try {
+            PreparedStatement ps = con.prepareStatement("select * from producto");
+            ResultSet rs = ps.executeQuery();
+            while(rs.next())
+            {
+                Object [] fila = new Object[7]; // Hay 6 columnas en la tabla
+                for (int i=0;i<7;i++)
+                fila[i] = rs.getObject(i+1); // El primer indice en rs es el 1, no el cero, por eso se suma 1.
+                modelo.addRow(fila);
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(ventanaVendedor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        tablaproducto.setModel(modelo);
+        
+    }
+     
+     public void limpiar(){
+         txtclave.setText(null);
+         txtdescripcion.setText(null);
+         txtexistencia.setText(null);
+         txtprecio.setText(null);
+     }
+     
     
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -218,9 +332,9 @@ public class ventanaGerente extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btagregarp;
-    private javax.swing.JButton btbuscar;
+    private javax.swing.JButton btagregar;
     private javax.swing.JButton bteliminar;
+    private javax.swing.JButton btlimpiar;
     private javax.swing.JComboBox<String> cbxbodega;
     private javax.swing.JComboBox<String> cbxlinea;
     private javax.swing.JLabel jLabel1;
@@ -230,7 +344,9 @@ public class ventanaGerente extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTabbedPane jTabbedPane1;
+    private javax.swing.JTable tablaproducto;
     private javax.swing.JTextField txtclave;
     private javax.swing.JTextField txtdescripcion;
     private javax.swing.JTextField txtexistencia;
